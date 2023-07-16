c3f7c87d4e4e0412785f9253047763167e6f86b6
VoidKeishi <phananha4@gmail.com>
2023-07-16 22:12:45 +0700

    Remove cv2, only use matplotlib

--- App.py
+++ App.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from Model import load_model
 from PIL import Image
-import cv2
 import numpy as np
 import io
 import os
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from urllib import request
 from keras.preprocessing import image_dataset_from_directory
 
@@ -15,6 +15,7 @@ def load_unet():
     if 'unet_model_weight_gg.h5' not in os.listdir('Weights'):
         print('Downloading...')
         request.urlretrieve(url, 'Weights/unet_model_weight_gg.h5')
+
 def load_unet():
     model = load_model()
     return model
@@ -48,31 +49,35 @@ if uploaded_file is not None:
     segmented_image_gray = np.squeeze(segmented_image)  # Remove batch dimension if present
     segmented_image_gray = np.uint8(segmented_image_gray * 255)
 
-    # Apply the color map to the grayscale image
-    heatmap = cv2.applyColorMap(segmented_image_gray, cv2.COLORMAP_JET)
-
-    # Convert the input image to uint8
-    image_uint8 = np.uint8(image[0] * 255)
-
-    # Apply the heatmap overlay on the original image
-    overlayed_image = cv2.addWeighted(image_uint8, 0.5, heatmap, 0.5, 0)
-
     # Create a binary black and white view by rounding the pixel values
     binary_image = np.round(segmented_image)
 
     # Convert binary image to PIL Image
     binary_image_pil = Image.fromarray(np.squeeze(binary_image).astype('uint8') * 255).convert('1')
 
-    c1, c2, c3, c4 = st.columns(4) 
-    
+    # Calculate the heat map
+    heatmap = segmented_image_gray.copy()
+
+    # Create a color map for the heat map
+    cmap = plt.cm.jet
+
+    # Plot the heat map overlay
+    fig, ax = plt.subplots()
+    ax.imshow(image[0], cmap='gray')
+    ax.imshow(heatmap, cmap=cmap, alpha=0.5)
+    ax.axis('off')
+    plt.tight_layout()
+
+    # Display the images and text
+    c1, c2, c3, c4 = st.columns(4)
+
     c1.image(image[0], channels='RGB')
     c1.text('Input Image')
-    
+
     c2.image(segmented_image[0], channels='L')
     c2.text('Segmented Image')
-    
+
     c3.image(binary_image_pil, channels='L')
     c3.text('Binary Image')
-    
-    c4.image(overlayed_image, channels='RGB')
-    c4.text('Heatmap Overlay')
+
+    c4.pyplot(fig)
